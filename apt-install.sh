6292354a1dbc7adf40ab318f7b6c5155f45625ef
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-10-02 18:35:55 +0300

    some missing deps

--- apt-install.sh
+++ apt-install.sh
@@ -13,11 +13,11 @@ if [ $retcode != 0 ]; then
     sudo -E ./llvm.sh 13
 fi
 
-sudo apt-get install libllvm-13-ocaml-dev libllvm13 llvm-13 llvm-13-dev llvm-13-doc llvm-13-examples llvm-13-runtime
-sudo apt-get install clang-13 clang-tools-13 clang-13-doc libclang-common-13-dev libclang-13-dev libclang1-13 clang-format-13 python-clang-13 clangd-13
+sudo apt-get install libllvm13 llvm-13 llvm-13-dev llvm-13-doc llvm-13-examples llvm-13-runtime
+sudo apt-get install clang-13 clang-tools-13 clang-13-doc libclang-common-13-dev libclang-13-dev libclang1-13 clang-format-13 clangd-13
 sudo apt-get install libclc-13-dev
 
-sudo apt-get install emacs jhbuild python3-pip screen libxml2-dev libxcb-glx0-dev libxcb-damage0-dev libxcb-present-dev libxshmfence-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libepoxy-dev libgtk-3-dev
+sudo apt-get install emacs jhbuild python3-pip screen libxml2-dev libxcb-glx0-dev libxcb-damage0-dev libxcb-present-dev libxshmfence-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libepoxy-dev libgtk-3-dev python3-ply
 sudo apt-get remove meson
 
 pip3 install meson
--- build-all.sh
+++ build-all.sh
@@ -2,3 +2,4 @@
 
 ./mesa-jhbuild build --nodeps mesa
 ./mesa-jhbuild build --nodeps Vulkan-ValidationLayers
+./mesa-jhbuild build --nodeps llvm-spirv
