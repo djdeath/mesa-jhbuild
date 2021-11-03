02b709fe334a07bb1f9d438e52e24abfe5b94650
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-11-03 21:20:14 +0200

    more missing packages

--- apt-install.sh
+++ apt-install.sh
@@ -22,7 +22,9 @@ sudo apt-get install libllvm13 llvm-13 llvm-13-dev llvm-13-doc llvm-13-examples 
 sudo apt-get install clang-13 clang-tools-13 clang-13-doc libclang-common-13-dev libclang-13-dev libclang1-13 clang-format-13 clangd-13
 sudo apt-get install libclc-13-dev
 
-sudo apt-get install emacs jhbuild python3-pip screen libxml2-dev libxcb-glx0-dev libxcb-damage0-dev libxcb-present-dev libxshmfence-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libepoxy-dev libgtk-3-dev python3-ply libembree-dev
+sudo apt-get install emacs jhbuild python3-pip screen libxml2-dev libxcb-glx0-dev libxcb-damage0-dev \
+     libxcb-present-dev libxshmfence-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libepoxy-dev \
+     libgtk-3-dev python3-ply libembree-dev libpciaccess-dev libtool cmake flex libxxf86vm-dev bison
 sudo apt-get remove meson
 
 pip3 install meson
