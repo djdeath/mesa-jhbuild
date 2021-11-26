7c7a1c8d3d820e5e70d15cc4a4963418be32533d
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2021-11-26 11:40:14 +0200

    add waffle

--- apt-install.sh
+++ apt-install.sh
@@ -24,7 +24,8 @@ sudo apt-get install libclc-13-dev
 
 sudo apt-get install emacs jhbuild python3-pip screen libxml2-dev libxcb-glx0-dev libxcb-damage0-dev \
      libxcb-present-dev libxshmfence-dev libx11-xcb-dev libxcb-dri2-0-dev libxcb-dri3-dev libepoxy-dev \
-     libgtk-3-dev python3-ply libembree-dev libpciaccess-dev libtool cmake flex libxxf86vm-dev bison valgrind
+     libgtk-3-dev python3-ply libembree-dev libpciaccess-dev libtool cmake flex libxxf86vm-dev bison \
+     valgrind libwaffle-dev
 sudo apt-get remove meson
 
 pip3 install meson
