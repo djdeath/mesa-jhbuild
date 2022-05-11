6e34323480a26eb81c242c5de7a06e491c8d6cdf
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2022-05-11 09:16:53 +0300

    env var update

--- mesa-env.sh
+++ mesa-env.sh
@@ -1,4 +1,4 @@
 export VK_ICD_FILENAMES=$HOME/mesa/build/share/vulkan/icd.d/intel_icd.x86_64.json
-export ANV_ENABLE_PIPELINE_CACHE=0
+export VK_ENABLE_PIPELINE_CACHE=0
 export MESA_VK_ABORT_ON_DEVICE_LOSS=1
 ./mesa-jhbuild shell
