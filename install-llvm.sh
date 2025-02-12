706698951203013f1fe06c96fe7d7fd75a3b65a8
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2025-02-12 13:45:31 +0200

    add libclc

--- install-llvm.sh
+++ install-llvm.sh
@@ -2,4 +2,4 @@
 
 ver=$1
 
-sudo apt-get install llvm-spirv-$ver llvm-$ver-dev libllvmspirvlib-$ver-dev libclang-$ver-dev libclang-common-$ver-dev
+sudo apt-get install llvm-spirv-$ver llvm-$ver-dev libllvmspirvlib-$ver-dev libclang-$ver-dev libclang-common-$ver-dev libclc-$ver-dev
